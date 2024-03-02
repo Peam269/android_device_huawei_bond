cc7bf47fa73be5870835d95a0f9aac49d3392b9c
Emil <emilwagenplast@gmail.com>
2024-03-02 19:43:01 +0100

    bond: Import odm init file
    * Taken from BND-L21 9.1.0.164
    
    Co-authored-by: A2L5E0X1 <alexed25@protonmail.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@ $(call inherit-product, vendor/huawei/bond/bond-vendor.mk)
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
+# Init
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/rootdir/etc/bond.rc:$(TARGET_COPY_OUT_ODM)/etc/init/bond.rc
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
